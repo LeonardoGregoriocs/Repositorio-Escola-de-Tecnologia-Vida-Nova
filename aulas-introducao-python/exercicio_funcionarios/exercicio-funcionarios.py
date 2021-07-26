e01a4785ccb1fa0059953709fc21f92e7187b66a
LeonardoGregoriocs <Leonardogregoriocs@gmail.com>
2021-07-26 19:48:17 -0300

    improvement in functions

--- aulas-introducao-python/exercicio_funcionarios/exercicio-funcionarios.py
+++ aulas-introducao-python/exercicio_funcionarios/exercicio-funcionarios.py
@@ -1,20 +1,47 @@
 import os 
 
-def dadosPessoais () -> None: #coletando dados pessoais dos usuarios.
+global salario
+
+#Ler o nome, sobrenome e cargo de um funcionário e retornar essas informações dentro de um vetor.
+def dadosPessoais () -> None: 
     nome = input('Digite seu nome: ')
     sobrenome = input('Digite seu sobrenome: ')
     cargo = input('Digite seu cargo: ')
     tabela = [nome, sobrenome, cargo]
     return tabela
 
-def informacoesFuncionario(nome, sobrenome, cargo): #imprimindo as informacoes na tela
+#Exibir na tela as informações do funcionário que devem ser passadas por parâmetro.
+def informacoesFuncionario(nome, sobrenome, cargo):
     os.system('cls')
     print('Olá, %s %s!\nSeu cargo é: %s' %(nome, sobrenome, cargo))
 
-def salarioFuncionario() -> None: #solicitando o salario do funcionario
-    salario = (input('\nDigite seu salário para calcularmos seu imposto de renda: '))
+#Ler e retornar um salário para esse funcionário.
+def salarioFuncionario() -> None:
+    global salario
+    salario = float(input('\nDigite seu salário para calcularmos seu imposto de renda: '))
+    return salario
+
+#Calcular e exibir na tela o salário Líquido desse funcionário depois de calcular o Imposto de Renda.
+def calculoImpostoDeRenda(salario: float):
+        if salario <= 1903.68:
+            print('\nVocê é isento, seu salário líquido é: ', salario)
+
+        elif salario > 1903.68 and salario <= 2826.65:
+            print('\nSeu salário líquido é de: ', salario - (salario * 0.075)) 
+            
+           
+        elif salario > 2826.65 and salario <= 3751.06:
+            print('\nSeu salário líquido é de: ', salario - (salario * 0.15))
+  
+            
+        elif salario > 3751.06 and salario <= 4664.68:
+            print('\nSeu salário líquido é de: ', salario - (salario * 0.225))
+
+        elif salario > 4664.68:
+            print('\nSeu salário líquido é de: ', salario - (salario * 0.275))
 
 
 var1 = dadosPessoais()
 informacoesFuncionario(var1[0], var1[1], var1[2])
 var2 = salarioFuncionario()
+calculoImpostoDeRenda(salario)
