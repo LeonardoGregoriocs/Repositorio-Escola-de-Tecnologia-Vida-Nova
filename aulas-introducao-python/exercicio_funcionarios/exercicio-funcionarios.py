77250db80c8d8b8e5b33b09f4cd6f3b6ff151719
LeonardoGregoriocs <Leonardogregoriocs@gmail.com>
2021-07-26 19:23:45 -0300

    adding comments

--- aulas-introducao-python/exercicio_funcionarios/exercicio-funcionarios.py
+++ aulas-introducao-python/exercicio_funcionarios/exercicio-funcionarios.py
@@ -1,17 +1,17 @@
-import os
+import os 
 
-def dadosPessoais () -> None:
+def dadosPessoais () -> None: #coletando dados pessoais dos usuarios.
     nome = input('Digite seu nome: ')
     sobrenome = input('Digite seu sobrenome: ')
     cargo = input('Digite seu cargo: ')
     tabela = [nome, sobrenome, cargo]
     return tabela
 
-def informacoesFuncionario(nome, sobrenome, cargo):
+def informacoesFuncionario(nome, sobrenome, cargo): #imprimindo as informacoes na tela
     os.system('cls')
     print('Olá, %s %s!\nSeu cargo é: %s' %(nome, sobrenome, cargo))
 
-def salarioFuncionario() -> None:
+def salarioFuncionario() -> None: #solicitando o salario do funcionario
     salario = (input('\nDigite seu salário para calcularmos seu imposto de renda: '))
 
 
