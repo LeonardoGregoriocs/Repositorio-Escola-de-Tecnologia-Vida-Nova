52783a172a4fba427ab288d9f13b4a8440f1a17b
LeonardoGregoriocs <Leonardogregoriocs@gmail.com>
2021-07-27 09:58:07 -0300

    improvement while

--- aulas-introducao-python/exercicio_funcionarios/exercicio-funcionarios.py
+++ aulas-introducao-python/exercicio_funcionarios/exercicio-funcionarios.py
@@ -23,22 +23,23 @@ def salarioFuncionario() -> None:
 
 #Calcular e exibir na tela o salário Líquido desse funcionário depois de calcular o Imposto de Renda.
 def calculoImpostoDeRenda(salario: float):
-        if salario <= 1903.68:
-            print('\nVocê é isento, seu salário líquido é: ', salario)
+    if salario <= 1903.68:
+        print('\nVocê é isento, seu salário líquido é: ', salario)
 
-        elif salario > 1903.68 and salario <= 2826.65:
-            print('\nSeu salário líquido é de: ', salario - (salario * 0.075)) 
-            
-           
-        elif salario > 2826.65 and salario <= 3751.06:
-            print('\nSeu salário líquido é de: ', salario - (salario * 0.15))
-  
-            
-        elif salario > 3751.06 and salario <= 4664.68:
-            print('\nSeu salário líquido é de: ', salario - (salario * 0.225))
+    elif salario > 1903.68 and salario <= 2826.65:
+        print('\nSeu salário líquido é de: ', salario - (salario * 0.075)) 
+        
+        
+    elif salario > 2826.65 and salario <= 3751.06:
+        print('\nSeu salário líquido é de: ', salario - (salario * 0.15))
 
-        elif salario > 4664.68:
-            print('\nSeu salário líquido é de: ', salario - (salario * 0.275))
+        
+    elif salario > 3751.06 and salario <= 4664.68:
+        print('\nSeu salário líquido é de: ', salario - (salario * 0.225))
+
+    elif salario > 4664.68:
+        print('\nSeu salário líquido é de: ', salario - (salario * 0.275))
+
 
 while True: 
     print('\nBem-vindo!\n\n1)Dados Pessoais \n2)Calcular Imposto de Renda\n3)Digite 0 para fechar o programa\n')
@@ -61,4 +62,18 @@ while True:
 
     elif(operacao == 0):
         print('Seu programa foi encerrado, você digitou 0!')
+        break
+        
+    while True:
+        print('\n1)Realizar um aumento no salário\n2)Voltar ao inicio\n3)Digite 0 para fechar o programa')
+        operacao2 = int(input('Favor selecionar a opção desejada: '))
+        os.system('cls')
+
+        if(operacao2 == 1):
+            salario = salario+float(input('\nDigite seu aumento/correção: '))
+            calculoImpostoDeRenda(salario)
+
+
+        elif(operacao == 0):
+            print('Seu programa foi encerrado, você digitou 0!')
         break
