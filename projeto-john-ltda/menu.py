4a57bad07012f4b1baa7a410e5c9c2c5a14c1196
LeonardoGregoriocs <Leonardogregoriocs@gmail.com>
2021-09-01 21:07:14 -0300

    Ultima alteracao do dia

--- projeto-john-ltda/menu.py
+++ projeto-john-ltda/menu.py
@@ -5,8 +5,8 @@ from motorista import Motoristas
 lista_de_carro = []
 
 while True:
-    
-    print('\n#------------------------#   \n1)Criar um veículo \n2)Ver lista de carro: ')
+
+    print('\n#------------------------#   \n1)Criar um veículo: \n2)Ver lista de veículo: \n3)Remover um veículo: 4)Mudar a cor do carro: ')
     opcao = int(input('\nDigite a opção desejada: '))
     
     os.system('cls')
@@ -16,5 +16,15 @@ while True:
         lista_de_carro.append(carro)
 
     elif(opcao == 2):
-        x = int(input('Digite a posição que deseja vizualizar: '))
-        print(lista_de_carro[x].print())
+        x = int(input('Qual veículo deseja visualizar: '))
+        print(lista_de_carro[x-1].print())
+
+    elif(opcao == 3):
+        x = int(input('Qual veículo deseja excluir: '))
+        lista_de_carro.pop(x)
+
+    elif(opcao == 4):
+        cor_nova = input('Digite a cor desejada: ')
+        #CRIAR UMA FUNÇÃO NA CLASSE
+
+    
