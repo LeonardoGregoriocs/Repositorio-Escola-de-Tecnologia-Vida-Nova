4dd81ff7ba9549e6ab9b74fc8dbfdc76e3127f72
LeonardoGregoriocs <Leonardogregoriocs@gmail.com>
2021-09-05 23:37:44 -0300

    Alteracao

--- projeto-john-ltda/menu.py
+++ projeto-john-ltda/menu.py
@@ -5,12 +5,11 @@ from motorista import Motoristas
 lista_de_carro = []
 
 while True:
-
-    print('\n#------------------------# \n1)Criar um veículo: \n2)Ver lista de veículo: \n3)Remover um veículo: \n4)Mudar a cor do carro: ')
+    print('\n#------------------------# PROJETO JOHN-LTDA #------------------------#')
+    print('\n1) Criar um veículo: \n2) Ver lista de veículo: \n3) Remover um veículo: \n4) Mudar a cor do carro: \n\n-> Digite zero para sair do programa\n')
+    print('#-----------------------------------------------------------------------#')
     opcao = int(input('\nDigite a opção desejada: '))
-    
-    os.system('cls')
-    
+
     if(opcao == 1):
         carro = Carro()
         lista_de_carro.append(carro)
@@ -26,4 +25,10 @@ while True:
     elif(opcao == 4):
         x = int(input('Qual a posição do veículo que deseja alterar a cor: '))
         lista_de_carro[x-1] = carro.trocar_cor()
+
+    #elif(opcao == 0): 
+    #    break
+
+    else:
+        print('Digite um número válido!')
         
