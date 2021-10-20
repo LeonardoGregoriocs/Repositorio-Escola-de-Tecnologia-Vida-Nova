48a52d3e68d6b9a591bc4f0bc5f1ab762f130a07
LeonardoGregoriocs <Leonardogregoriocs@gmail.com>
2021-10-19 23:00:11 -0300

    database

---
+++ criando-banco-de-dados-python/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///mydatabase.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo = True)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+Base = declarative_base()
+
+class Pessoa(Base): 
+    __tablename__ = "pessoas"
+
+    id= Column(Integer,primary_key=True)
+    name= Column(String)
+    documents= Column(String, unique=True)
+    phonenumber= Column(String)
+
+
+Base.metadata.create_all(engine)
+
+p1 = Pessoa(name="Leonardo", documents="12345678998", phonenumber="19988885565")
+p2 = Pessoa(name="Edivane", documents="98765432112", phonenumber="1987541236")
+p3 = Pessoa(name="William", documents="12345678998", phonenumber="14985236541")
+p2 = Pessoa(name="Thiago", documents="14785236988", phonenumber="1987541236")
+
+session.add(p1)
+session.add(p2)
+
+session.commit()
